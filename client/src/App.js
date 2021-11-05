dcce7cd0c4fbc77187c0bae645abb4e94a2b1845
Younus Ali Akash <younusaliakash8@gmail.com>
2021-11-06 02:01:07 +0600

    add uusid

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+import {
+  BrowserRouter as Router, Navigate, Route, Routes
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 import TextEditor from './Components/TextEditor/TextEditor';
 
 
 function App() {
   return (
-    <div>
-      <TextEditor/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/document/${uuidV4()}`}/>}/>
+        <Route path="/document/:id" element={<TextEditor/>} /> 
+      </Routes>
+    </Router>
   );
 }
 
